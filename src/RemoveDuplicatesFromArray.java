085d0de9da19884cbc4ddbc5a4736c50d90a2213
Alessandro <alessandro.zanzottera13@gmail.com>
2022-02-19 10:42:38 +0100

    Remove Duplicates From Array easy solved

---
+++ src/RemoveDuplicatesFromArray.java
@@ -0,0 +1,17 @@
+public class RemoveDuplicatesFromArray {
+    public static void main(String[] args) {
+        RemoveDuplicatesFromArray removeDuplicatesFromArray = new RemoveDuplicatesFromArray();
+        int[] nums = {1, 1, 2};
+        System.out.println(removeDuplicatesFromArray.removeDuplicates(nums));
+    }
+
+    public int removeDuplicates(int[] nums) {
+        int result = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[result]) {
+                nums[++result] = nums[i];
+            }
+        }
+        return result + 1;
+    }
+}
