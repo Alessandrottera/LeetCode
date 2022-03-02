5b35a59962b6df18f46bcbdc82684e511bf74e1e
alessandro.zanzotter <alessandro.zanzottera@csttech.it>
2022-03-02 14:46:11 +0100

    Add Binary

--- src/leetCode/easy/AddBinary.java
+++ src/leetCode/easy/AddBinary.java
@@ -1,25 +1,21 @@
 //Given two binary strings a and b, return their sum as a binary string.
 package leetCode.easy;
 
+import java.math.BigInteger;
+
 public class AddBinary {
 
     public static void main(String args[]){
         String a = "11";
-        String b = "1";
+        String b = "11";
         AddBinary addBinary = new AddBinary();
         System.out.println(addBinary.addBinarySolution(a, b));
     }
 
 
     public String addBinarySolution(String a, String b) {
-        // I create result name string and initially it is empuyt end in the end I'm going to return it as our answer
-        StringBuilder res = new StringBuilder();
-        int i = a.length() - 1; // i create i variable for string and I have to start adding from right to left
-        int j = b.length() - 1; // similar variable j for string b
-        int carry = 0;
-        //
-
-
-        return null;
+        BigInteger d1 = new BigInteger(a, 2);
+        BigInteger d2 = new BigInteger(b, 2);
+        return (d1.add(d2)).toString(2);
     }
 }
